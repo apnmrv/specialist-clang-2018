e020aeea554e54a293389a2c323413b8ee09de89
apnmrv <apnmrv@gmail.com>
2018-04-28 13:58:14 +0300

    изменено:      cl07_Greetings/main.c 	новый файл:    cl09_SwitchWeekDays/main.c

--- cl07_Greetings/main.c
+++ cl07_Greetings/main.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 int main() {
-	char firstName[10];
-	char lastName[20];
+	char firstName[256];
+	char lastName[256];
 	printf("Input your first name: ");
-	scanf("%s",&firstName);
+	scanf("%s",firstName);
 	printf("Input your last name: ");
-	scanf("%s",&lastName);
+	scanf("%s",lastName);
 
-	printf("Hello, %s %s!", firstName, lastName);
+	printf("Hello, %s %s!\n", firstName, lastName);
 
 	return 0;
 }
