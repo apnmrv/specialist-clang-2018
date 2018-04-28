208400d941b1a83fe47e8cfb205fd31a84d0d43b
apnmrv <apnmrv@gmail.com>
2018-04-28 14:08:03 +0300

    изменено:      cl09_SwitchWeekDays/main.c

--- cl09_SwitchWeekDays/main.c
+++ cl09_SwitchWeekDays/main.c
@@ -6,8 +6,24 @@ int main() {
 	int dayNumber;
 	
 	printf("Input day number : ");
-	scanf(%d, dayNumber);
+	scanf("%d", &dayNumber);
 	
+	switch(dayNumber){
+		case 1 : printf("Sunday\n");
+			break;
+		case 2 : printf("Monday\n");
+			break;
+		case 3 : printf("Tuesday\n");
+			break;
+		case 4 : printf("Wednesday\n");
+			break;
+		case 5 : printf("Thursday\n");
+			break;
+		case 6 : printf("Saturday\n");
+			break;
+		
+		default: printf("Wrong number!\n");
+	}
 
 	return 0;
 } 
