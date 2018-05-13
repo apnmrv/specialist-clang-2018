92672e03bba2f8deafb3a97f226815cb2ed9781f
apnmrv <apnmrv@gmail.com>
2018-05-13 12:10:48 +0300

    BubbleSortSimpe added

---
+++ cl15_BubbleSortSimple/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_SIZE 100
+
+int main(){
+	
+	float arr[MAX_SIZE], x;
+	int i, j, n;
+	
+	srand (time(NULL));
+	printf("Input array's capacity :  ");
+	scanf("%d", &n);
+	
+	
+	// Data acquisition
+	
+	printf("\nRandom data : \n");
+	for (i=0; i<n; i++) {
+		arr[i] = rand()%100/10.;
+		printf("%.1f ", arr[i]);
+	}
+	
+	// Sorting
+	for (i=n; i>1; i--){
+		for (j=1; j<i; j++) {
+			if (arr[j] < arr[j-1]) {
+				x = arr[j];
+				arr[j] = arr[j-1];
+				arr[j-1] = x;
+			}				
+		}
+	}
+	
+	// Result output
+	
+	printf("\nSorted data : \n");
+	for (i=0; i<n; i++) {
+		printf("%.1f ", arr[i]);
+	}
+	
+	return 0;
+}
